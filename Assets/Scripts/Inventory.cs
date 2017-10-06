f7a600e35a2cda54f93fc38a8ae58751320effa2
Fred Smith <Fred Smith>
2017-10-06 14:10:36 +0100

    Updated Inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    ItemDatabase database;
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+    int slotAmount;
+
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    private void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+
+        slotAmount = 25;
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.Find("Slot Panel").gameObject;
+        for (int i = 0; i < slotAmount; i++)
+        {
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(0);
+        AddItem(1);
+
+        Debug.Log(items[1].Title);
+
+    }
+
+    public void AddItem(int id)
+    {
+        Item itemToAdd = database.FetchItemByID(id);
+        for(int i = 0; i< items.Count; i++)
+        {
+            if(items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.transform.SetParent(slots[i].transform);
+                itemObj.transform.position = Vector2.zero;
+                itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObj.name = itemToAdd.Title;
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using System.IO;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+    private List<Item> database = new List<Item>();
+    private JsonData itemData;
+
+
+    void Start()
+    {
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        ConstructItemDatabase();
+
+        Debug.Log(FetchItemByID(0).Description);
+    }
+
+    public Item FetchItemByID(int id)
+    {
+        for( int i =0; i < database.Count; i++)
+            if (database[i].ID == id)
+                return database[i];
+        
+        return null;
+    }
+
+    void ConstructItemDatabase()
+    {
+        for(int i =0; i < itemData.Count; i++)
+        {
+            database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"],
+                (int)itemData[i]["stats"]["power"], (int)itemData[i]["stats"]["defence"], (int)itemData[i]["stats"]["weight"],
+                itemData[i]["description"].ToString(),(bool)itemData[i]["stackable"],(int)itemData[i]["rarity"],
+                itemData[i]["slug"].ToString()
+                ));
+        }
+    }
+}
+
+public class Item {
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public int Power { get; set; }
+    public int Defence { get; set; }
+    public int Weight { get; set; }
+    public string Description { get; set; }
+    public bool Stackable { get; set; }
+    public int Rarity { get; set; }
+    public string Slug { get; set; }
+    public Sprite Sprite { get; set; }
+
+    public Item(int id, string title, int value, int power, int defence, int weight, string description,
+        bool stackable, int rarity, string slug)
+    {
+        this.ID = id;
+        this.Title = title;
+        this.Value = value;
+        this.Power = power;
+        this.Defence = defence;
+        this.Weight = weight;
+        this.Description = description;
+        this.Stackable = stackable;
+        this.Rarity = rarity;
+        this.Slug = slug;
+        this.Sprite = Resources.Load<Sprite>("Sprites/Items/"+ slug);
+    }
+
+    public Item()
+    {
+        this.ID = -1;
+    }
+}
